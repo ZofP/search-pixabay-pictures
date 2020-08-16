d94b42f541f766024900eedc1c4be6c0afaf909c
PrincePetya <petr.zofak@gmail.com>
2020-08-16 13:55:29 +0200

    changed displaying tags into unordered list

--- src/components/card/card.component.jsx
+++ src/components/card/card.component.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 
-import "./card.styles.css";
+import "./card.styles.scss";
 
 const Card = ({
   picture: { previewURL, tags, webformatURL, user },
   showPicture,
 }) => {
+  const tagsArray = tags.split(",").filter(Boolean);
+
   return (
     <div className="card-container">
       <div
@@ -14,7 +16,12 @@ const Card = ({
       >
         <img src={previewURL} alt="pixabay" />
       </div>
-      <h2>tags: {tags}</h2>
+      <h4>tags:</h4>
+      <ul>
+        {tagsArray.map((tag) => {
+          return <li key={tag}>{tag}</li>;
+        })}
+      </ul>
       <p>user: {user}</p>
     </div>
   );
